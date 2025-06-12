dc8005447246c5c01fa3f1c4339055b5d2559859
Ayoub <ayoubjarhni@gmail.com>
2025-06-12 22:02:00 +0100

    Initial commit

---
+++ src/main/java/com/example/signatureQR/SignatureQrApplication.java
@@ -0,0 +1,13 @@
+package com.example.signatureQR;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SignatureQrApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SignatureQrApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/signatureQR/SignatureQrApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.signatureQR;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SignatureQrApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
